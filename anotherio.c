3c5f2b623151f12ff5912560eb53d7a654320195
Chan Kang <kentakang@icloud.com>
2017-11-27 10:11:24 +0900

    Add anotherio.c
    Study putchar, getchar and puts, gets

---
+++ anotherio.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+	char s[100];
+	char c = 'h';
+	putchar(c);
+	putchar(73);
+	
+	puts("");
+	
+	puts("Hello");
+	
+	//c = getchar();
+	//putchar(c);
+	
+	gets(s);
+	puts(s);
+	
+	return 0;
+}
